c5e8f8aae70762c6408c7063b14dbcbd48f5e0c4
Nguyen Cong Thanh <thanhproe2k2@gmail.com>
2023-08-11 22:24:27 +0700

    upload new 11/8/2023

--- test.py
+++ test.py
@@ -2,3 +2,13 @@ print("Hello, world!")
 firstName = "Nguyen Cong"
 lastName = "Thanh"
 print(firstName + " " + lastName)
+
+def sum(arr):
+    sum = 0
+    for n in arr:
+        sum += n
+    return sum
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+res = sum(arr)
+print("Result of arr: ", res)
